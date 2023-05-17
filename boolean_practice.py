556072d0c47df0f2fe667c48f639fa94e4ec2611
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-05-17 13:36:53 -0500

    booleans and ifs

---
+++ boolean_practice.py
@@ -0,0 +1,18 @@
+# b = True
+# c = False
+# d = 'true'
+# type(b)
+# type(d)
+
+# 2 > 3
+# #false
+
+# 2 < 3
+
+num1 = 100
+num2 = 500
+
+if num1 > num2:
+    print('num 1 is bigger than num2')
+else:
+    print('num2 is bigger than num1')
