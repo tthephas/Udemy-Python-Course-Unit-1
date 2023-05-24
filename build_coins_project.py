3e06e34ae2a393020d9466c03549f8b932851c6a
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-05-23 21:51:09 -0500

    setup constructor for all coins

--- build_coins_project.py
+++ build_coins_project.py
@@ -3,7 +3,12 @@
 import random 
 
 class Coin:
-    def __init__(self, rare=False, clean=True, heads=True):
+    def __init__(self, rare=False, clean=True, heads=True, **kwargs):
+
+        for key,value in kwargs.items():
+            setattr(self,key,value)
+            
+
         self.is_rare = rare
         self.is_clean = clean
         self.heads = heads
@@ -33,3 +38,15 @@ class Coin:
         self.heads = choice
     
 
+class Pound(Coin):
+    def __init__(self):
+        data = {
+            'original_value': 1.00,
+            'clean_color':'gold',
+            'rusty_color':'greenish',
+            'num_edges': 1,
+            'diameter': 22.5,
+            'thickness': 3.15,
+            'mass': 9.5
+        }
+        super().__init__(**data)
