10d5c484973db66f200dfe0c09d7afbdb5c4dcef
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-05-23 21:33:16 -0500

    built coin class with methods and destructor

--- classes_practice.py
+++ classes_practice.py
@@ -1,9 +1,38 @@
 # classes
+import random
 
 class Pound:
-    value = 1.00
-    color = 'gold'
-    num_edges = 1
-    diameter = 22.5 ##mm
-    thickness = 3.15 #mm
-    heads = True
+
+    def __init__(self, rare=False):
+
+        self.rare = rare
+
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+
+        self.value = 1.00
+        self.color = 'gold'
+        self.num_edges = 1
+        self.diameter = 22.5 ##mm
+        self.thickness = 3.15 ##mm
+        self.heads = True
+
+    def rust(self):
+        self.color = 'greenish'
+
+    def clean(self):
+        self.color = 'gold'
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+    
+    def __del__(self):
+        print('Coin Spent!')
+    
+    
+
+    
